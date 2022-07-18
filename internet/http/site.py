16cecc7756a38fc577fb7e5ee8587658721a5cb9
Parham Alvani <parham.alvani@gmail.com>
2022-07-19 00:47:47 +0430

    feat: pass messages into login page

--- internet/http/site.py
+++ internet/http/site.py
@@ -1,12 +1,16 @@
 import sanic
 from sanic_ext import render
 
+from ..message.message import MESSAGES
+
 
 class SiteHandler():
     @staticmethod
     async def index(_: sanic.Request) -> sanic.HTTPResponse:
         return await render(
-            "index.html", context={}, status=200
+            "index.html", context={
+                'messages': MESSAGES,
+            }, status=200
         )
  
     def register(self) -> sanic.Blueprint:
--- internet/message/message.py
+++ internet/message/message.py
@@ -3,44 +3,12 @@ messages in english and persian to use on jinja templates.
 '''
 
 MESSAGES = {
-    'internet_login': {
-        'fa': 'ورود به اینترنت',
-        'en': 'Internet Login'
-    },
-    'login': {
-        'fa': 'ورود',
-        'en': 'Login'
-    },
-    'email': {
-        'fa': 'آدرس رایانامه',
-        'en': 'Email Address'
-    },
-    'password': {
-        'fa': 'رمز ورود',
-        'en': 'Password'
-    },
-    'no_id': {
-        'fa': 'شناسه کاربری ندارم',
-        'en': 'I have not username'
-    },
-    'guest_login': {
-        'fa': 'ورود بدون شناسه (سرعت پایین)',
-        'en': 'guest login (low speed)'
-    },
-    'forgot_password': {
-        'fa': 'رمز ورود خودم را فراموش کرده ام',
-        'en': 'forgot my password'
-    },
-    'change_password': {
-        'fa': 'تغییر رمز ورود',
-        'en': 'change password'
-    },
-    'other_lang': {
-        'fa': 'English',
-        'en': 'فارسی'
-    },
-    '_other_lang_key': {
-        'fa': 'en',
-        'en': 'fa'
-    }
+    'internet_login': 'ورود به اینترنت',
+    'login': 'ورود',
+    'email':  'آدرس رایانامه',
+    'password': 'رمز ورود',
+    'no_id': 'شناسه کاربری ندارم',
+    'guest_login': 'ورود بدون شناسه (سرعت پایین)',
+    'forgot_password': 'رمز ورود خودم را فراموش کرده ام',
+    'change_password': 'تغییر رمز ورود',
 }
