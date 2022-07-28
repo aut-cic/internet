f766f7bf73d52dc5e20af8c6ba31ef7a28708a4d
Parham Alvani <parham.alvani@gmail.com>
2022-07-28 13:58:22 +0430

    feat: add frontend with webpack

---
+++ frontend/src/index.ts
@@ -0,0 +1,8 @@
+import "bootstrap/dist/js/bootstrap";
+import "bootstrap/dist/css/bootstrap.rtl.min.css";
+
+import "vazirmatn/Vazirmatn-font-face.css";
+
+import "./app.css";
+
+window.console.log("hello world");
---
+++ frontend/webpack.config.js
@@ -0,0 +1,35 @@
+const webpack = require("webpack");
+const path = require("path");
+
+/*
+ * We've enabled UglifyJSPlugin for you! This minifies your app
+ * in order to load faster and run less javascript.
+ *
+ * https://github.com/webpack-contrib/uglifyjs-webpack-plugin
+ *
+ */
+
+module.exports = {
+  entry: "./src/index.ts",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+
+  module: {
+    rules: [
+      { test: /\.ts$/, use: "ts-loader" },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+        ],
+      },
+    ],
+  },
+};
