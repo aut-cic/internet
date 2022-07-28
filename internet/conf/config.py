708fe610582a1fda86ccbeaed58043f6f449cd63
Parham Alvani <parham.alvani@gmail.com>
2022-07-28 16:37:53 +0430

    feat: add login url into configuration

--- internet/conf/config.py
+++ internet/conf/config.py
@@ -1,6 +1,6 @@
-'''
+"""
 load configuration with validation.
-'''
+"""
 
 import dataclasses
 
@@ -11,44 +11,50 @@ from dynaconf import LazySettings, Validator
 class Database:
     host: str = "127.0.0.1"
     port: int = 3306
-    database: str = 'radius'
-    username: str = 'opnsense'
-    password: str = 'opnsense@123'
+    database: str = "radius"
+    username: str = "opnsense"
+    password: str = "opnsense@123"
 
 
 @dataclasses.dataclass()
 class Config:
     database: Database = Database()
+    login_url: str = "https://login.aut.ac.ir/login"
 
 
 def load() -> Config:
-    '''
+    """
     loads configuration with validation into Config.
     each new configuration must be validated and then set into config.
-    '''
+    """
     settings = LazySettings(
-        settings_file=['config.toml'],
+        settings_file=["config.toml"],
         envvar_prefix="INTERNET",
         nested_separator="__",
         validators=[
-            Validator('database.host', is_type_of=(str),
-                      default="127.0.0.1"),
-            Validator('database.port', is_type_of=(int),
-                      default=3306),
-            Validator('database.database', is_type_of=(str),
-                      default="radius"),
-            Validator('database.username', is_type_of=(str),
-                      default='opnsense'),
-            Validator('database.password', is_type_of=(str),
-                      default='opnsense@123'),
-        ]
+            Validator("database.host", is_type_of=(str), default="127.0.0.1"),
+            Validator("database.port", is_type_of=(int), default=3306),
+            Validator("database.database", is_type_of=(str), default="radius"),
+            Validator(
+                "database.username", is_type_of=(str), default="opnsense"
+            ),
+            Validator(
+                "database.password", is_type_of=(str), default="opnsense@123"
+            ),
+            Validator(
+                "login_url",
+                is_type_of=(str),
+                default="https://login.aut.ac.ir/login",
+            ),
+        ],
     )
 
     cfg = Config()
-    cfg.database.host = settings['database.host']
-    cfg.database.port = settings['database.port']
-    cfg.database.database = settings['database.database']
-    cfg.database.username = settings['database.username']
-    cfg.database.password = settings['database.password']
+    cfg.database.host = settings["database.host"]
+    cfg.database.port = settings["database.port"]
+    cfg.database.database = settings["database.database"]
+    cfg.database.username = settings["database.username"]
+    cfg.database.password = settings["database.password"]
+    cfg.login_url = settings["login_url"]
 
     return cfg
