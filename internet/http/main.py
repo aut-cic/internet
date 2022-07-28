15cac577a4f77100fa9e8588614425a49e7acbcb
Parham Alvani <parham.alvani@gmail.com>
2022-07-28 13:57:47 +0430

    feat: add static file server

--- internet/http/main.py
+++ internet/http/main.py
@@ -6,9 +6,13 @@ from .site import SiteHandler
 
 
 def app() -> sanic.Sanic:
-    app = sanic.Sanic('internet')
-    app.ctx.logger = logging.getLogger('internet.http')
+    app = sanic.Sanic("internet")
+    # logger in the context which is useful for handlers
+    app.ctx.logger = logging.getLogger("internet.http")
 
     app.blueprint(SiteHandler().register())
+
+    app.static("/static", "./frontend/dist", name="static")
+    app.static("/public", "./public", name="public")
 
     return app
