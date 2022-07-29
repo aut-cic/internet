b6f3812f8bbd4a9395e4837f7a39eb8ac0a498de
Parham Alvani <parham.alvani@gmail.com>
2022-07-29 13:37:40 +0430

    feat: add comments on accounting classes

--- internet/accounting/usage.py
+++ internet/accounting/usage.py
@@ -4,6 +4,11 @@ import datetime
 
 @dataclasses.dataclass
 class UsageRecord:
+    """
+    daily records based on what we have on radius database,
+    this used as a basis for generating other reports.
+    """
+
     discount: int
     usage: int
     created_date: datetime.date
@@ -11,6 +16,10 @@ class UsageRecord:
 
 @dataclasses.dataclass
 class Session:
+    """
+    each session is a successful login.
+    """
+
     ip: str
     id: str
     time: datetime.datetime
@@ -21,6 +30,10 @@ class Session:
 
 @dataclasses.dataclass
 class Usage:
+    """
+    usage is aggregated download/upload in the last month in bytes.
+    """
+
     daily: int = 0
     weekly: int = 0
     monthly: int = 0
@@ -28,6 +41,11 @@ class Usage:
 
 @dataclasses.dataclass
 class Package:
+    """
+    package specify the available download/upload volume
+    based on your group name in GB.
+    """
+
     daily_volume: int = 0
     weekly_volume: int = 0
     monthly_volume: int = 0
@@ -35,6 +53,10 @@ class Package:
 
 @dataclasses.dataclass
 class Report:
+    """
+    aggregated information for generating the status page.
+    """
+
     usage_history: list[UsageRecord]
     package: Package
     groupname: str
