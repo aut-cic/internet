469012cb204371b6ae45a0bb95290b1984ecbef2
Parham Alvani <parham.alvani@gmail.com>
2022-07-29 10:18:32 +0430

    feat: add hands-on testing

--- internet/accounting/acct.py
+++ internet/accounting/acct.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta
 from typing import Any
 
 from sanic_ext.utils.typing import typing
-from sqlalchemy import select
+from sqlalchemy import create_engine, select
 from sqlalchemy.orm import Session
 
 from ..model.radacct import RadiusAccount
@@ -69,4 +69,17 @@ class AccountingService:
         row = self.session.execute(statement).first()
         if row is None:
             return None
-        return row.username
+        return row.RadiusAccount.username
+
+
+# for hands-on testing on production environment with radius database.
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+pymysql://root:root@127.0.0.1/db",
+        echo=True,
+        future=True,
+    )
+
+    with Session(engine) as session:
+        usage = AccountingService(session)
+        usage.user_usage("parham.alvani")
