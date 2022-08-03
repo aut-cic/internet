31f645edcdab3afb789556324495a6d376c1f4b3
Parham Alvani <parham.alvani@gmail.com>
2022-08-03 13:51:38 +0430

    feat: update frontend package

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,7 +1,9 @@
 import "bootstrap/dist/js/bootstrap";
 import "bootstrap/dist/css/bootstrap.rtl.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 import "vazirmatn/Vazirmatn-font-face.css";
+import "vazirmatn/misc/Farsi-Digits/Vazirmatn-FD-font-face.css";
 
 import "chart.js/dist/chart.min.js";
 
