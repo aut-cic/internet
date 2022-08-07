148a201afa0bfd42510449e0d5e082823c141b0c
Parham Alvani <parham.alvani@gmail.com>
2022-08-07 08:58:16 +0430

    feat: add announcement package

---
+++ internet/announcements/__init__.py
@@ -0,0 +1,1 @@
+from .announcements import list
---
+++ internet/announcements/announcements.py
@@ -0,0 +1,38 @@
+'''
+announcements package reads announcements
+from the file.
+'''
+
+import dataclasses
+import json
+import typing
+
+
+@dataclasses.dataclass()
+class Announcement:
+    '''
+    Announcement show an announcement on the status page.
+    '''
+    type: str
+    status: bool
+    message: str
+
+
+__announcements: list[Announcement] = []
+if len(__announcements) == 0:
+    with open("announcements/announcements.json", encoding="utf-8") as fp:
+        for announcement in json.load(fp):
+            __announcements.append(
+                Announcement(
+                    type=announcement["type"],
+                    status=announcement["status"],
+                    message=announcement["message"],
+                )
+            )
+
+
+def list() -> typing.Iterator[Announcement]:
+    '''
+    list returns an iterator for all the announcements
+    '''
+    return iter(__announcements)
