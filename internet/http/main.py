d02a976d3647949f6e6699240fdfe8b206757f59
Parham Alvani <parham.alvani@gmail.com>
2022-08-12 23:59:00 +0430

    Revert "feat: configuration for behind-proxy"
    This reverts commit 84254c3dc6020557d695bd67b3311d83743ba9d3.

--- internet/http/main.py
+++ internet/http/main.py
@@ -5,18 +5,15 @@ from .site import SiteHandler
 from .status import StatusHandler
 
 
-def app(
-    login_url: str, logout_url: str, behind_proxy: bool, engine: Engine
-) -> sanic.Sanic:
+def app(login_url: str, logout_url: str, engine: Engine) -> sanic.Sanic:
     app = sanic.Sanic("internet")
     app.ctx.login_url = login_url
     app.ctx.logout_url = logout_url
     app.ctx.engine = engine
 
-    if behind_proxy is True:
-        # configuration for using internet service behind nginx
-        app.config.PROXIES_COUNT = 1
-        app.config.REAL_IP_HEADER = "x-real-ip"
+    # configuration for using internet service behind nginx
+    app.config.PROXIES_COUNT = 1
+    app.config.REAL_IP_HEADER = "x-real-ip"
 
     app.blueprint(SiteHandler().register())
     app.blueprint(StatusHandler().register())
