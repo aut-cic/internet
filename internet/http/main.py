631bd5ee13b81890cb5f449fa94d6b5a343abf1b
Parham Alvani <parham.alvani@gmail.com>
2022-08-12 21:48:30 +0430

    feat: add proxy configuration

--- internet/http/main.py
+++ internet/http/main.py
@@ -10,6 +10,8 @@ def app(login_url: str, logout_url: str, engine: Engine) -> sanic.Sanic:
     app.ctx.login_url = login_url
     app.ctx.logout_url = logout_url
     app.ctx.engine = engine
+    app.config.PROXIES_COUNT = 1
+    app.config.REAL_IP_HEADER = "x-real-ip"
 
     app.blueprint(SiteHandler().register())
     app.blueprint(StatusHandler().register())
