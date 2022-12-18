b52c0fed596def6d53aee2919969c9cd883fb0e2
Parham Alvani <parham.alvani@gmail.com>
2022-12-18 22:32:21 +0330

    feat: show popup with user status page

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,6 +1,7 @@
 import "bootstrap/dist/css/bootstrap.rtl.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./bootstrap.scss";
+import * as bootstrap from "bootstrap/dist/js/bootstrap.min.js";
 
 import "vazirmatn/Vazirmatn-font-face.css";
 import "vazirmatn/misc/Farsi-Digits/Vazirmatn-FD-font-face.css";
@@ -12,3 +13,11 @@ import "./login.ts";
 
 window.console.log("https://github.com/aut-cic/internet");
 window.console.log("AUT internet controller frontend and backend");
+
+window.onload = () => {
+  let modal = window.document.getElementById("packageModal");
+  if (modal != null) {
+    new bootstrap.Modal(modal).show();
+    window.console.log("Hello, We have modal");
+  }
+};
