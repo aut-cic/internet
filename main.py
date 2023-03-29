a14d6e471ed625e3ae98efc1e6b5b80abd2d6f5b
Parham Alvani <parham.alvani@gmail.com>
2023-03-29 09:54:12 +0330

    feat: handle metrics for multiprocess system like sanic

--- main.py
+++ main.py
@@ -2,15 +2,17 @@
 runs internet server
 """
 
+import os
 from rich import pretty
 from sqlalchemy import create_engine
+from sanic import raw
 import prometheus_client
+import prometheus_client.multiprocess
 
 import internet.conf
 import internet.http.main
 import internet.subnets
 import internet.announcements
-
 
 pretty.install()
 
@@ -32,10 +34,19 @@ engine = create_engine(
 
 app = internet.http.main.create_app(cfg.login_url, cfg.logout_url, engine)
 
+os.environ["PROMETHEUS_MULTIPROC_DIR"] = "./prom"
+
+
+@app.route("/metrics", methods=["GET"])
+async def expose_metrics(_):
+    registry = prometheus_client.CollectorRegistry()
+    prometheus_client.multiprocess.MultiProcessCollector(registry)
+    data = prometheus_client.generate_latest(registry)
+    return raw(data, content_type=prometheus_client.CONTENT_TYPE_LATEST)
+
+
 if __name__ == "__main__":
     # start the Prometheus metrics server
-    prometheus_client.start_http_server(1373)
-
     app.run(
         host=cfg.listen.host,
         port=cfg.listen.port,
